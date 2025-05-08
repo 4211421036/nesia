cc35d8ad14138de8b68ec7381fa088cada8840a5
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-05-08 22:10:09 +0700

    Update ast.c

--- src/ast.c
+++ src/ast.c
@@ -1,9 +1,8 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "ast.h"
-
-#define _POSIX_C_SOURCE 200809L
 
 AstNode* createPrintNode(char *msg, AstNode *expr) {
     AstNode *node = malloc(sizeof(AstNode));
