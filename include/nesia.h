f4fc1405a55affc691836bfe9c32ffe6953a9938
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-05-08 21:27:59 +0700

    Create nesia.h

---
+++ include/nesia.h
@@ -0,0 +1,13 @@
+#ifndef NESIA_H
+#define NESIA_H
+
+// Versi Nesia
+#define NESIA_VERSION "0.1.0"
+
+// Maksimum variabel global
+#define MAX_VAR 256
+
+// Panjang maksimum nama variabel/fungsi
+#define MAX_NAME_LEN 64
+
+#endif // NESIA_H
