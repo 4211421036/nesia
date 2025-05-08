7a9a695827a68ca411627665138c5c0fdb1774f7
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-05-08 22:06:27 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,36 +1,35 @@
-// main.c
 #include <stdio.h>
 #include <stdlib.h>
 #include "error.h"
 #include "cli.h"
+#include "parser.h"
+#include "interpreter.h"
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Penggunaan: nesia <berkas.ns>\n");
         return 1;
     }
-    char *source = malloc(len + 1);
     const char *filename = argv[1];
-    // Baca seluruh file ke memori
+    
     FILE *fp = fopen(filename, "r");
     if (!fp) {
         fprintf(stderr, "Tidak dapat membuka berkas '%s'\n", filename);
         return 1;
     }
     fseek(fp, 0, SEEK_END);
-    long len = ftell(fp);
+    long len = ftell(fp); // Deklarasi 'len' dipindah ke sini
     fseek(fp, 0, SEEK_SET);
-    char *source = malloc(len+1);
+    char *source = malloc(len + 1);
     fread(source, 1, len, fp);
     source[len] = '\0';
     fclose(fp);
 
-    // Analisis leksikal & sintaks
     Parser parser;
     initParser(&parser, source);
     AstNode *program = parseProgram(&parser);
 
-    // Eksekusi AST
     interpret(program);
+    free(source);
     return 0;
 }
