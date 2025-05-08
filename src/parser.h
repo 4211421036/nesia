771df6281afafb311129aa23421d17f2765b1a75
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-05-08 21:43:41 +0700

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -1,16 +1,13 @@
-// parser.h
 #ifndef PARSER_H
 #define PARSER_H
 
 #include "lexer.h"
 #include "ast.h"
 
-typedef struct {
-    Lexer lexer;
-    Token current;
-} Parser;
-
 void initParser(Parser *parser, const char *source);
 AstNode* parseProgram(Parser *parser);
+// Tambahan:
+void parserError(int line, const char *msg);
+AstNode* parseExpression(Parser *parser);
 
 #endif
